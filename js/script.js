d71a14d953b74e1fc75fcaa4e3c80fbd04c3551d
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-19 21:08:52 -0400

    feat: implement and test getHumanChoice() function
    Added implementation for getHumanChoice() function
    and added a simple test that would prompt the user
    for a choice three times and output the choice to
    the console each time a valid input was received.

--- js/script.js
+++ js/script.js
@@ -22,4 +22,25 @@ function getHumanChoice() {
     3. If input is a valid choice (rock, paper, or scissors), return that choice.
     4. If input is not a valid choice, continue prompting until a valid choice is entered.
     */
+
+   while (true) {
+        let choice = prompt('Rock, Paper, or Scissors?', '');
+        
+        switch(choice.toLowerCase()) {
+            case 'rock':
+                return 'Rock';
+            case 'paper':
+                return 'Paper';
+            case 'scissors':
+                return 'Scissors';
+            default:
+                console.log('Invalid input! Please choose either Rock, Paper, or Scissors');
+                break;
+        }
+   }
+}
+
+for (let i = 0; i < 3; i++) {
+    let choice = getHumanChoice();
+    console.log(choice);
 }
