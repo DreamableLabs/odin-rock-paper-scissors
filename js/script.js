743a1790d6145b1e5a7544c1eea5b18ce21fb05b
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-19 20:53:07 -0400

    feat - implement and test getComputerChoice() function
    Added implementation for getComputerChoice() function
    and added a script that would run the function 10
    million times and print the number of times each of
    the three options (rock, paper, scissors) was chosen
    to ensure that each is selected about one third of
    the time.

--- js/script.js
+++ js/script.js
@@ -1,8 +1,42 @@
-console.log('Hello World!')
-
 function getComputerChoice() {
     /*
     1. Randomly choose an integer from 1 to 3, inclusive.
     2. Based on the integer chosen, return 'Rock', 'Paper', or 'Scissors'.
     */
+   let choice = Math.floor(Math.random()*3) + 1;
+   switch(choice) {
+    case 1: 
+        return 'Rock';
+        break;
+    case 2:
+        return 'Paper';
+        break;
+    case 3:
+        return 'Scissors';
+        break;
+    default:
+        alert('Error in calculating computer choice!')
+   }
 }
+
+let rock_count = 0;
+let paper_count = 0;
+let scissors_count = 0;
+for (let i = 0; i < 10000000; i++) {
+    switch(getComputerChoice()) {
+        case 'Rock':
+            rock_count++;
+            break;
+        case 'Paper':
+            paper_count++;
+            break;
+        case 'Scissors':
+            scissors_count++;
+            break;
+    }
+}
+
+console.log(`Rock count: ${rock_count}`);
+console.log(`Paper count: ${paper_count}`);
+console.log(`Scissors count: ${scissors_count}`);
+
