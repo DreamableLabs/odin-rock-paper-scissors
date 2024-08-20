cf8fa2a1cf596c4120dcecf83b1e8dacecb680a1
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-19 20:56:32 -0400

    chore(cleanup): remove test script and function comments

--- js/script.js
+++ js/script.js
@@ -1,8 +1,4 @@
 function getComputerChoice() {
-    /*
-    1. Randomly choose an integer from 1 to 3, inclusive.
-    2. Based on the integer chosen, return 'Rock', 'Paper', or 'Scissors'.
-    */
    let choice = Math.floor(Math.random()*3) + 1;
    switch(choice) {
     case 1: 
@@ -19,24 +15,4 @@ function getComputerChoice() {
    }
 }
 
-let rock_count = 0;
-let paper_count = 0;
-let scissors_count = 0;
-for (let i = 0; i < 10000000; i++) {
-    switch(getComputerChoice()) {
-        case 'Rock':
-            rock_count++;
-            break;
-        case 'Paper':
-            paper_count++;
-            break;
-        case 'Scissors':
-            scissors_count++;
-            break;
-    }
-}
-
-console.log(`Rock count: ${rock_count}`);
-console.log(`Paper count: ${paper_count}`);
-console.log(`Scissors count: ${scissors_count}`);
 
