36e5a562da0aeec888426eb868bac1f7c9140ad3
Alex <1936228+DreamableLabs@users.noreply.github.com>
2024-08-19 20:44:41 -0400

    feat - add pseudocode for getComputerChoice() function

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
 console.log('Hello World!')
+
+function getComputerChoice() {
+    /*
+    1. Randomly choose an integer from 1 to 3, inclusive.
+    2. Based on the integer chosen, return 'Rock', 'Paper', or 'Scissors'.
+    */
+}
